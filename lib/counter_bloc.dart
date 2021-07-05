44864f12310b241bfda31d8c6b7fe4c9f13a743a
Vysakh R J <vysakh.rj@neoito.com>
2021-07-05 08:10:29 +0530

    initial commit

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+
+class CounterBloc {
+  int counter = 0;
+
+  final _stateStreamController = StreamController<int>();
+  StreamSink<int> get counterSink => _stateStreamController.sink;
+  Stream<int> get counterStream => _stateStreamController.stream;
+
+  final _eventStreamController = StreamController<CounterAction>();
+  StreamSink<CounterAction> get eventSink => _eventStreamController.sink;
+  Stream<CounterAction> get eventStream => _eventStreamController.stream;
+
+  CounterBloc() {
+    counter = 0;
+    eventStream.listen((event) {
+      if (event == CounterAction.Increment) {
+        counter++;
+      } else if (event == CounterAction.Decrement) {
+        counter--;
+      } else if (event == CounterAction.Reset) {
+        counter = 0;
+      }
+      counterSink.add(counter);
+    });
+  }
+}
+
+enum CounterAction { Increment, Decrement, Reset }
